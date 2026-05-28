efa9922b1ac1b0b147f290f56d8bb1b0983d9de7
Keterlyn <keterlyn.moreno@gmail.com>
2026-05-28 14:25:09 -0500

    sincronizando base de tablas

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,8 +9,10 @@ const PORT = process.env.PORT;
 
 async function startServer() {
     await sequelize.authenticate();
-
     console.log("Base de datos conectada");
+
+    await sequelize.sync({ alter: true });
+    console.log("Tablas sincronizadas");
 
     app.listen(PORT, ()=>{
         console.log(`Conectado al puerto ${PORT}`);
